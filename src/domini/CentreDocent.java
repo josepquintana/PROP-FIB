d7d16b41df273b9251d340fb66535a2933b71781
Josep Quintana <josep.quintana.44@gmail.com>
2018-10-27 05:26:01 +0200

    Centre docent (#1)
    * Struct classes
    
    * Added constuctors, setters, getters and Date and Time implementations
    
    * Added constuctors, setters, getters and Date and Time implementations

--- src/domini/CentreDocent.java
+++ src/domini/CentreDocent.java
@@ -1,9 +1,67 @@
 package domini;
 
 
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.Time;
+import java.util.Date;
+
 public class CentreDocent
 {
     private String nomCentre;
-    private
+    private PeriodeLectiu periodeLectiu;
+    private JornadaLectiva jornadaLectiva;
 
+    /**
+     * @param nomCentre
+     * @param periodeLectiu
+     * @param jornadaLectiva
+     */
+    public CentreDocent(String nomCentre, PeriodeLectiu periodeLectiu, JornadaLectiva jornadaLectiva) {
+        this.nomCentre = nomCentre;
+        this.periodeLectiu = new PeriodeLectiu(periodeLectiu);
+        this.jornadaLectiva = new JornadaLectiva(jornadaLectiva);
+    }
+
+    public CentreDocent(@NotNull CentreDocent cd) {
+        this.nomCentre = cd.getNomCentre();
+        this.periodeLectiu = new PeriodeLectiu(cd.getPeriodeLectiu());
+        this.jornadaLectiva = new JornadaLectiva(cd.getJornadaLectiva());
+    }
+
+//    public void AssignarAulaACentreDocent(Aula aula) {
+//
+//    }
+
+    public String getNomCentre() {
+        return this.nomCentre;
+    }
+
+    public PeriodeLectiu getPeriodeLectiu() {
+        return this.periodeLectiu;
+    }
+
+    public JornadaLectiva getJornadaLectiva() {
+        return this.jornadaLectiva;
+    }
+
+    public void setNomCentre(String nomCentre) {
+        this.nomCentre = new String(nomCentre);
+    }
+
+    public void setPeriodeLectiu(PeriodeLectiu periodeLectiu) {
+        this.periodeLectiu = new PeriodeLectiu(periodeLectiu);
+    }
+
+    public void setJornadaLectiva(JornadaLectiva jornadaLectiva) {
+        this.jornadaLectiva = new JornadaLectiva(jornadaLectiva);
+    }
+
+    public void printCentreDocent() {
+        System.out.println("> CentreDocent:");
+        System.out.println(" nomCentre: " + this.nomCentre);
+        this.periodeLectiu.printPeriodeLectiu();
+        this.jornadaLectiva.printJornadaLectiva();
+    }
 }
+
---
+++ src/domini/PeriodeLectiu.java
@@ -0,0 +1,42 @@
+package domini;
+
+import org.jetbrains.annotations.NotNull;
+import java.util.Date;
+
+public class PeriodeLectiu
+{
+    private Date dataIni;
+    private Date dataFi;
+
+    public PeriodeLectiu(Date dataIni, Date dataFi) {
+        this.dataIni = new Date(dataIni.getTime());
+        this.dataFi  = new Date(dataFi.getTime());
+    }
+
+    public PeriodeLectiu(@NotNull PeriodeLectiu pL) {
+        this.dataIni = new Date(pL.getDataIni().getTime());
+        this.dataFi  = new Date(pL.getDataFi().getTime());
+    }
+
+    public void setDataIni(Date dataIni) {
+        this.dataIni = new Date(dataIni.getTime());
+    }
+
+    public void setDataFi(Date dataFi) {
+        this.dataFi = new Date(dataFi.getTime());
+    }
+
+    public Date getDataIni() {
+        return dataIni;
+    }
+
+    public Date getDataFi() {
+        return dataFi;
+    }
+
+    public void printPeriodeLectiu() {
+        System.out.println(" DataIni: " + this.getDataIni());
+        System.out.println(" DataFi : " + this.getDataFi());
+    }
+
+}
