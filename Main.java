10df2f2a6510ad1caf4b711fd5cec0fcb5cefcfd
josep.quintana <josep.quintana.44@gmail.com>
2018-10-31 00:42:05 +0100

    Centre done

--- Main.java
+++ Main.java
@@ -1,8 +1,6 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.Date;
+import java.net.Inet4Address;
+import java.util.*;
 import java.sql.Time;
 
 import domini.*;
@@ -28,34 +26,71 @@ public class Main
 
     // Execution-control variables
     public boolean printLongFormat = true;
-    static boolean printCentreDocent = true;
+    static boolean printCentreDocent = false;
     static boolean all = true;
 
     public static void main(String[] args) throws Exception
     {
         openFile();
 
-        while ((op = readNextLine()) != null) {
-            // evaluate command
-            evaluateCommand(op);
-        }
+        while ((op = readNextLine()) != null) { evaluateCommand(op); }
 
         closeFile();
 
-        crearCentreDocent();
-        crearAules();
-        assignarAules();
-        crearAssignatures();
-        crearPlansDeEstudis();
-        assignarPlansDeEstudis();
+        //crearCentreDocent();
+        //crearAules();
+        //assignarAules();
+        //crearAssignatures();
+        //crearPlansDeEstudis();
+        //assignarPlansDeEstudis();
 
         if (printCentreDocent) cd.printCentreDocentLong();
 
     }
 
-    private static void evaluateCommand(String op) {
+    private static void evaluateCommand(String op) throws Exception {
 
+        try {
+            System.out.println(op);
+            Scanner s = new Scanner(op).useDelimiter(", ");
+
+            String tipo = s.next();
+            if (tipo.equals("Centre Docent")) {
+                String nomCentre = s.next();
+
+                Time horaIni = new Time(Integer.parseInt(s.next()), Integer.parseInt(s.next()), 0);
+                Time horaFi  = new Time(Integer.parseInt(s.next()), Integer.parseInt(s.next()), 0);
+
+                int day = Integer.parseInt(s.next());
+                String month = s.next(); // !!! Calcular be
+                int year = Integer.parseInt(s.next());
+                Date dataIni = new GregorianCalendar(year, Calendar.SEPTEMBER, day, horaIni.getHours(), horaIni.getMinutes()).getTime();
+
+                day = Integer.parseInt(s.next());
+                month = s.next(); // !!! Calcular be
+                year = Integer.parseInt(s.next());
+                Date dataFi  = new GregorianCalendar(year, Calendar.JANUARY, day, horaIni.getHours(), horaIni.getMinutes()).getTime();
+
+                crearCentreDocent(nomCentre, dataIni, dataFi, horaIni, horaFi);
+                cd.printCentreDocent();
+            }
+            else if (tipo.equals("Aula")) {
+
+            }
+            else if (tipo.equals("Assignatura")) {
+
+            }
+            else if (tipo.equals("Pla Esutdis")) {
+
+            }
+        }
+
+        catch (Exception e) {
+            //System.out.println("> !Error al evaluar comanda del fitxer");
+            System.out.println(e.getMessage());
+        }
     }
+
 
     public static void openFile() throws IOException {
 
@@ -81,15 +116,15 @@ public class Main
         return strLine;
     }
 
-    public static void crearCentreDocent() {
-        String nomCentre = new String("FIB");
+    public static void crearCentreDocent(String nomCentre, Date dataIni, Date dataFi, Time horaIni, Time horaFi) {
+//        String nomCentre = new String("FIB");
 
-        Time horaIni = new Time(8, 0, 0);
-        Time horaFi =  new Time(20, 0, 0);
+//        Time horaIni = new Time(8, 0, 0);
+//        Time horaFi =  new Time(20, 0, 0);
         JornadaLectiva jornadaLectiva = new JornadaLectiva(horaIni, horaFi);
 
-        Date dataIni = new GregorianCalendar(2018, Calendar.SEPTEMBER, 06, horaIni.getHours(), horaIni.getMinutes()).getTime();
-        Date dataFi  = new GregorianCalendar(2019, Calendar.JANUARY, 28, horaFi.getHours(), horaFi.getMinutes()).getTime();
+//        Date dataIni = new GregorianCalendar(2018, Calendar.SEPTEMBER, 06, horaIni.getHours(), horaIni.getMinutes()).getTime();
+//        Date dataFi  = new GregorianCalendar(2019, Calendar.JANUARY, 28, horaFi.getHours(), horaFi.getMinutes()).getTime();
         PeriodeLectiu periodeLectiu = new PeriodeLectiu(dataIni, dataFi);
 
         cd = new CentreDocent(nomCentre, periodeLectiu, jornadaLectiva);
