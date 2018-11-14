ec9a2f7d93075d7dbf1ac0e9da2295ee844d69c8
Donadeu <43960130+Donadeu@users.noreply.github.com>
2018-11-14 17:24:47 +0100

    Add files via upload

--- drivers/DriverGrup/DriverGrup.java
+++ drivers/DriverGrup/DriverGrup.java
@@ -1,3 +1,5 @@
+
+package domini;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
@@ -15,15 +17,21 @@ public class DriverGrup {
             while (!sortir) {
                 System.out.println("Tria una opció (número) seguit dels paràmetres necessaris per comprovar el mètode:");
                 System.out.println("\t 1) Grup()");
-                System.out.println("\t 2) Grup(String codi, int num, int capacitat)");
+                System.out.println("\t 2) Grup(String codi, int num, int capacitat, int numSubGrups, int hores)");
                 System.out.println("\t 3) Grup(Grup g) (hauràs d'introduir els paràmetres necessaris per cridar la constructora de la opció 2)");
                 System.out.println("\t 4) boolean equals(Grup g) (hauràs d'introduir els paràmetres necessaris per cridar la constructora de la opció 2)");
                 System.out.println("\t 5) String getCodiAssig()");
                 System.out.println("\t 6) int getCapacitat()");
                 System.out.println("\t 7) int getNumGrup()");
-                System.out.println("\t 8) void setCodiAssig(String s)");
-                System.out.println("\t 9) void setNumGrup(int n)");
-                System.out.println("\t 10) void setCapacitat(int n)");
+                System.out.println("\t 8) int getHoresLab()");
+                System.out.println("\t 9) int getSubGrups()");
+                System.out.println("\t 10) int getCapacitatSub()");
+                System.out.println("\t 11) void setCodiAssig(String s)");
+                System.out.println("\t 12) void setNumGrup(int n)");
+                System.out.println("\t 13) void setCapacitat(int n)");
+                System.out.println("\t 14) void setCapacitatSub(int n)");
+                System.out.println("\t 15) void setHoresLab()");
+                System.out.println("\t 16) void restarHoraLab()");
                 System.out.println("\t 0) Sortir");
 
                 String linea;
@@ -40,17 +48,19 @@ public class DriverGrup {
                             G = new Grup();
                             break;
                         case "2":
-                            String codi = (paraules[1]);
-                            Integer num = Integer.parseInt(paraules[2]);
-                            Integer capacitat = Integer.parseInt(paraules[3]);
-                            G = new Grup(codi, num, capacitat);
+                            String codi = paraules[1];
+                            int num = Integer.parseInt(paraules[2]);
+                            int capacitat = Integer.parseInt(paraules[3]);
+                            int numsubgrups = Integer.parseInt(paraules[4]);
+                            int hores = Integer.parseInt(paraules[5]);        
+                            G = new Grup(codi, num, capacitat, numsubgrups, hores);
                             break;
                         case "3":
-                            Grup A = new Grup(paraules[1], Integer.parseInt(paraules[2]), Integer.parseInt(paraules[3]));
+                            Grup A = new Grup(paraules[1], Integer.parseInt(paraules[2]), Integer.parseInt(paraules[3]), Integer.parseInt(paraules[4]), Integer.parseInt(paraules[5]));
                             G = new Grup(A);
                             break;
                         case "4":
-                            Grup B = new Grup(paraules[1], Integer.parseInt(paraules[2]), Integer.parseInt(paraules[3]));
+                            Grup B = new Grup(paraules[1], Integer.parseInt(paraules[2]), Integer.parseInt(paraules[3]), Integer.parseInt(paraules[4]), Integer.parseInt(paraules[5]));
                             System.out.println("Comparació del grup amb codi assig: " + G.getCodiAssig() + " i número de grup: " + G.getNumGrup() + " amb el grup amb codi assig: " + B.getCodiAssig() + " i número de grup: " + B.getNumGrup() + ": " + G.equals(B));
                             break;
                         case "5":
@@ -63,14 +73,32 @@ public class DriverGrup {
                             System.out.println("Número del grup actual: " + G.getNumGrup() + ".");
                             break;
                         case "8":
-                            G.setCodiAssig(paraules[1]);
+                            System.out.println("Hores de laboratori del grup actual: " + G.getHoresLab() + ".");
                             break;
                         case "9":
-                            G.setNumGrup(Integer.parseInt(paraules[1]));
+                            System.out.println("Subgrups del grup actual: " + G.getSubGrups() + ".");
                             break;
                         case "10":
+                            System.out.println("Capacitat dels subgrups del grup actual: " + G.getCapacitatSub() + ".");
+                            break;    
+                        case "11":
+                            G.setCodiAssig(paraules[1]);
+                            break;
+                        case "12":
+                            G.setNumGrup(Integer.parseInt(paraules[1]));
+                            break;
+                        case "13":
                             G.setCapacitat(Integer.parseInt(paraules[1]));
-                            break;     
+                            break;
+                        case "14":
+                            G.setCapacitatSub(Integer.parseInt(paraules[1]));
+                            break;
+                        case "15":
+                            G.setHoresLab();
+                            break;
+                        case "16":
+                            G.restarHoraLab();
+                            break;    
                         case "0":
                             sortir = true;
                             break;
