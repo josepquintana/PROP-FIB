6de000b81efe710aea1068dcaa55ac4d59a4fb4d
josep.quintana <josep.quintana.44@gmail.com>
2018-12-15 19:19:19 +0100

    Main.java calls PresentationCtrl

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import domini.MyException;
+import presentacio.ControladorPresentacio;
+
+public class Main
+{
+    public static void main(String[] args) throws Exception
+    {
+        javax.swing.SwingUtilities.invokeLater (
+        new Runnable() {
+        public void run() {
+            ControladorPresentacio ctrlPresentacion = new ControladorPresentacio();
+            ctrlPresentacion.inicialitzarPresentacio();
+        }
+        });
+    }
+}
