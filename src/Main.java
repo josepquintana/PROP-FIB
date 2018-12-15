7448d42ff665fb83d46fc59d93f4528c38d42c92
Donadeu <43960130+Donadeu@users.noreply.github.com>
2018-12-15 14:41:37 +0100

    Actualitzat el main pq cridi al ctrl presentacio

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,16 @@
-import domini.ControladorDomini;
 import domini.MyException;
+import presentacio.ControladorPresentacio;
 
 public class Main
 {
     public static void main(String[] args) throws Exception
     {
-        printHello();
-        ControladorDomini cd;
-        cd = new ControladorDomini();
-        String inputFileName  = args[0];        // filename for the data input
-
-        cd.readInputFile(inputFileName);
-
-
-        for (int i = 0; i < cd.getPlansDeEstudis().mida(); i++) {
-            cd.generateHorariPlaEstudis(i);
-            cd.printHorari(i);
+        javax.swing.SwingUtilities.invokeLater (
+        new Runnable() {
+        public void run() {
+            ControladorPresentacio ctrlPresentacion = new ControladorPresentacio();
+            ctrlPresentacion.inicialitzarPresentacio();
         }
-
+        });
     }
-
-    private static void printHello() {
-        System.out.print("\n");
-        System.out.println("##########################################################################################");
-        System.out.println("############################### PROP: GENERADOR DE HORARIS ###############################");
-        System.out.println("##########################################################################################");
-        System.out.print("\n");
-    }
-
 }
