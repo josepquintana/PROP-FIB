c48490552f45fda56f930fb5475bb6066f563657
josep.quintana <josep.quintana.44@gmail.com>
2018-12-15 20:01:14 +0100

    .

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,11 @@ public class Main
     public static void main(String[] args) throws Exception
     {
         javax.swing.SwingUtilities.invokeLater (
-        new Runnable() {
-        public void run() {
-            ControladorPresentacio ctrlPresentacion = new ControladorPresentacio();
-            ctrlPresentacion.inicialitzarPresentacio();
-        }
-        });
+                new Runnable() {
+                    public void run() {
+                        ControladorPresentacio ctrlPresentacion = new ControladorPresentacio();
+                        ctrlPresentacion.inicialitzarPresentacio();
+                    }
+                });
     }
 }
